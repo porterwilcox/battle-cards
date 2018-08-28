472f500d618c7ef67a9b611dcc174ce3048acdb0
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-28 17:12:47 -0600

    deck select and post game to server

--- src/store.js
+++ src/store.js
@@ -1,16 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+
+let gameApi = Axios.create({
+  baseURL: `https://inspire-server.herokuapp.com/cards`,
+  timeout: 3000
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    aGame:{}
   },
   mutations: {
-
+    setGame(state, game){
+      state.aGame = game
+    }
   },
   actions: {
-
+    selectDeck({dispatch, commit}, deckNum){
+      let gameObj
+      switch(deckNum){
+        case 1:
+        gameObj = {"playerName": "robots", "set": 1}
+        break
+        case 2:
+        gameObj = {"playerName": "spinner hat heads", "set": 2}
+        break
+        case 3:
+        gameObj = {"playerName": "robot heads", "set": 3}
+        break
+        case 4:
+        gameObj = {"playerName": "cat pack", "set": 4}
+        break
+      }
+      gameApi.post('/', {"gameConfig": gameObj})
+        .then(res => commit('setGame', res.data))
+    }
   }
 })
